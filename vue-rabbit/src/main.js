7fcbdbf12e88547a5bd340a0e6b5a428141a863f
jimmy <2482521248@qq.com>
2023-07-10 16:34:50 +0800

    图片懒加载基本实现

--- vue-rabbit/src/main.js
+++ vue-rabbit/src/main.js
@@ -1,6 +1,9 @@
 // 引入初始化的样式文件
 import '@/styles/common.scss'
 
+// 定义懒加载插件
+import { useIntersectionObserver } from '@vueuse/core'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -20,3 +23,22 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+app.directive('img-lazy', {
+    mounted(el, binding) {
+        // el:指令绑定的元素img
+        // binding:binding.value 指令等于号后面绑定的表达式的值 图片url
+        console.log(el, binding)
+        useIntersectionObserver(
+            el,
+            ([{ isIntersecting }]) => {
+                console.log(isIntersecting)
+                if (isIntersecting) {
+                    // 进入视口区域
+                    el.src = binding.value
+
+                }
+            },
+        )
+    }
+})
