aad3a845fdfc804d47270718b6e71bbfd833c7df
jimmy <2482521248@qq.com>
2023-07-10 16:46:06 +0800

    图片懒加载指令优化

---
+++ vue-rabbit/src/directives/index.js
@@ -0,0 +1,28 @@
+// 定义懒加载插件
+import { useIntersectionObserver } from '@vueuse/core'
+
+export const directivePlugin = {
+    install(app) {
+        // 使用app实现懒加载指令逻辑
+        app.directive('img-lazy', {
+            mounted(el, binding) {
+                // el:指令绑定的元素img
+                // binding:binding.value 指令等于号后面绑定的表达式的值 图片url
+                console.log(el, binding)
+                const { stop } = useIntersectionObserver(
+                    el,
+                    ([{ isIntersecting }]) => {
+                        console.log(isIntersecting)
+                        if (isIntersecting) {
+                            // 进入视口区域
+                            el.src = binding.value
+                            stop()
+
+                        }
+                    },
+                )
+            }
+        })
+    }
+}
+
--- vue-rabbit/src/main.js
+++ vue-rabbit/src/main.js
@@ -2,7 +2,7 @@
 import '@/styles/common.scss'
 
 // 定义懒加载插件
-import { useIntersectionObserver } from '@vueuse/core'
+import { directivePlugin } from '@/directives/index.js'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -21,24 +21,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(directivePlugin)
 app.mount('#app')
 
-app.directive('img-lazy', {
-    mounted(el, binding) {
-        // el:指令绑定的元素img
-        // binding:binding.value 指令等于号后面绑定的表达式的值 图片url
-        console.log(el, binding)
-        useIntersectionObserver(
-            el,
-            ([{ isIntersecting }]) => {
-                console.log(isIntersecting)
-                if (isIntersecting) {
-                    // 进入视口区域
-                    el.src = binding.value
 
-                }
-            },
-        )
-    }
-})
+
