075c169c065ae9606bbe347dd8c978b50b733dc8
jimmy <2482521248@qq.com>
2023-07-27 17:39:05 +0800

    登录-表单校验

--- vue-rabbit/src/views/Login/index.vue
+++ vue-rabbit/src/views/Login/index.vue
@@ -1,5 +1,25 @@
 <script setup>
+// 表单校验（账户名+密码）
+import { ref } from 'vue'
 
+// 1.准备表单对象
+const form = ref({
+    account: '',
+    password: ''
+})
+
+
+// 2.准备规则对象
+const rules = {
+    account: [
+        { required: true, message: '请输入账号名', trigger: 'blur' },
+    ],
+    password: [
+        { required: true, message: '请输入密码', trigger: 'blur' },
+        { min: 6, max: 14, message: '请输入长度在6~14的密码', trigger: 'blur' },
+
+    ]
+}
 </script>
 
 
@@ -24,12 +44,12 @@
                 </nav>
                 <div class="account-box">
                     <div class="form">
-                        <el-form label-position="right" label-width="60px" status-icon>
-                            <el-form-item label="账户">
-                                <el-input />
+                        <el-form :model="form" label-position="right" label-width="60px" status-icon :rules="rules">
+                            <el-form-item label="账户" prop="account">
+                                <el-input v-model="form.account" />
                             </el-form-item>
-                            <el-form-item label="密码">
-                                <el-input />
+                            <el-form-item label="密码" prop="password">
+                                <el-input v-model="form.password" type="password" />
                             </el-form-item>
                             <el-form-item label-width="22px">
                                 <el-checkbox size="large">
