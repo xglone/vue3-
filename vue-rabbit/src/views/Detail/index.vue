74bb0c579e7d22c16b3a3afc79d06f1da862aac7
jimmy <2482521248@qq.com>
2023-08-09 18:30:46 +0800

    本地购物车-添加购物车实现

--- vue-rabbit/src/views/Detail/index.vue
+++ vue-rabbit/src/views/Detail/index.vue
@@ -3,6 +3,9 @@ import { getDetail } from '@/apis/detail.js'
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import detailHot from '@/views/Detail/components/detailHot.vue'
+import { ElMessage } from 'element-plus';
+import { userCartStore } from '@/stores/cartStore';
+const cartStore = userCartStore()
 const goods = ref({})
 const route = useRoute()
 const getGoods = async () => {
@@ -13,8 +16,36 @@ const getGoods = async () => {
 onMounted(() => getGoods())
 
 // sku规格被操作时
+let skuObj = {}
 const skuChange = (sku) => {
     console.log(sku)
+    skuObj = sku
+}
+// count
+const count = ref(1)
+const countChange = (count) => {
+    console.log(count)
+}
+// 添加购物车
+const addCart = () => {
+    if (skuObj.skuId) {
+        // 规格已经选择 触发action
+        cartStore.addCart({
+            id: goods.value.id,//商品id
+            name: goods.value.name,//商品名称
+            picture: goods.value.mainPictures[0],//图片
+            price: goods.value.price,//最新价格
+            count: count.value,//商品数量
+            skuId: skuObj.skuId,//skuId
+            ottrsText: skuObj.specsText,//商品规格文本
+            selected: true,//商品是否选中
+
+        })
+
+    } else {
+        // 规格没有选择 提示用户
+        ElMessage.warning('请选择规格')
+    }
 }
 </script>
 
@@ -97,10 +128,11 @@ const skuChange = (sku) => {
                             <!-- sku组件 -->
                             <XglSku :goods="goods" @change="skuChange" />
                             <!-- 数据组件 -->
+                            <el-input-number v-model="count" @change="countChange" />
 
                             <!-- 按钮组件 -->
                             <div>
-                                <el-button size="large" class="btn">
+                                <el-button size="large" class="btn" @click="addCart">
                                     加入购物车
                                 </el-button>
                             </div>
