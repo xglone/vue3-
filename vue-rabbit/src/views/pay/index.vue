3460e7deb59ee2b242d72e1c461c23be847415da
jimmy <2482521248@qq.com>
2023-08-25 18:01:39 +0800

    支付-获取基础的支付信息

--- vue-rabbit/src/views/pay/index.vue
+++ vue-rabbit/src/views/pay/index.vue
@@ -1,5 +1,14 @@
 <script setup>
-const payInfo = {}
+import { getOrderAPI } from '@/apis/pay'
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const payInfo = ref({})
+const getpayInfo = async () => {
+    const res = await getOrderAPI(route.query.id)
+    payInfo.value = res.result
+}
+onMounted(() => getpayInfo())
 </script>
 
 
