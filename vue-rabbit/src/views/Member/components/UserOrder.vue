230973ae4ba6bee782c15d6b248d901ac716cff7
jimmy <2482521248@qq.com>
2023-08-26 11:35:33 +0800

    tab切换获取状态数据

--- vue-rabbit/src/views/Member/components/UserOrder.vue
+++ vue-rabbit/src/views/Member/components/UserOrder.vue
@@ -22,7 +22,11 @@ const tabTypes = [
     { name: "complete", label: "已完成" },
     { name: "cancel", label: "已取消" }
 ]
-
+// tab切换
+const tabChange = (type) => {
+    params.value.orderState = type
+    getOrderList()
+}
 onMounted(() => {
     getOrderList()
 })
@@ -30,7 +34,7 @@ onMounted(() => {
 
 <template>
     <div class="order-container">
-        <el-tabs>
+        <el-tabs @tab-change="tabChange">
             <!-- tab切换 -->
             <el-tab-pane v-for="item in tabTypes" :key="item.name" :label="item.label" />
 
